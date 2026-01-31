5e840873878c3fad3324da7a02594ec883b9b23c
ohhthatguy <thakullabhaskar456@gmail.com>
2026-01-31 19:42:47 +0545

    this time change in GET for deploy

--- app/api/redirect/route.ts
+++ app/api/redirect/route.ts
@@ -1,8 +1,8 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { cookies } from "next/headers";
 import jwt from "jsonwebtoken";
 
-export async function GET(req: NextResponse) {
+export async function GET(req: NextRequest) {
   try {
     const cookieStore = await cookies();
     const token = cookieStore.get("token")?.value || "";
@@ -10,13 +10,13 @@ export async function GET(req: NextResponse) {
     //first verify the token
     const verifiedToken = (await jwt.verify(
       token,
-      process.env.JWT_SECRET!
+      process.env.JWT_SECRET!,
     )) as { id: string; role: "guide" | "customer" };
 
     if (!verifiedToken)
       return NextResponse.json(
         { msg: "Token is not verified for routing" },
-        { status: 500 }
+        { status: 500 },
       );
 
     const data =
@@ -26,19 +26,19 @@ export async function GET(req: NextResponse) {
 
     return NextResponse.json(
       { msg: "succesfully retrived routing Data", data },
-      { status: 200 }
+      { status: 200 },
     );
   } catch (error) {
     console.log(
       "Error in reading cookkies to set the route while clicking the logo (backend): ",
-      error
+      error,
     );
     return NextResponse.json(
       {
         msg: "Error in reading cookkies to set the route while clicking the logo (backend): ",
         data: error,
       },
-      { status: 500 }
+      { status: 500 },
     );
   }
 }
